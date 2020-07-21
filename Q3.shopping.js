3c326d02d169290f94b55ea66c737b12261d4304
Gopinathan G <68248460+Gopinathan-G@users.noreply.github.com>
2020-07-21 05:33:34 +0530

    Day 4 Assignment
    Letsupgrade Javscript Day 4 Assignment submitted

---
+++ Q3.shopping.js
@@ -0,0 +1,15 @@
+//.image you are out to do some grocery shopping.
+//.so you have an array called shopping list with all the product you want to buy.
+let shoppinglist = ("peanut butter,all purpose flour,Bakery, Bread,Meat,Seafood,Pasta,Rice,Oils, Sauces and biscuits");
+console.log(shoppinglist )
+//.now that you are inside of the shop you have basket with all the product from the list but you want to add few more 
+//.create a new array called shopping basket that will be a copy of shoppingList array,
+let shoppingBasket = ("Dairy Milk,Cheese,Eggs,Groundnut,Mustard,wheat flour,ice cream.chocolate,tea,coffee,fruits,panneer,noodels");
+console.log(shoppingBasket)
+//.that will be copy of shoppinglist array and add some new product 
+let arr = [];
+arr.push("peanut butter,all purpose flour,Bakery, Bread,Meat,Seafood,Pasta,Rice,Oils, Sauces and biscuits");
+arr.push= ("Dairy Milk,Cheese,Eggs,Groundnut,Mustard,wheat flour,ice cream.chocolate,tea,coffee,fruits,panneer,noodels");
+console.log(arr);
+
+
---
+++ Q4.Calculator.js
@@ -0,0 +1,42 @@
+function AdditionBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function  SubtractionBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 - num2;
+}
+
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function SquarerootBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 ^ num2;
+}
+
+function PercentageBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 % num2;
+}
+
