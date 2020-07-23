82ab451355476961c9c2cd9f9499c9318cb40cac
Gopinathan G <68248460+Gopinathan-G@users.noreply.github.com>
2020-07-24 03:38:41 +0530

    Day 4 Assignment
    LetsUpgrade Javascript Day 4 Assignment

---
+++ Q6.loops.js
@@ -0,0 +1,10 @@
+// Write a loop which prompts for a number greater than 100. 
+// If the visitor enters another number – ask them to input again.
+// The loop must ask for a number until either the visitor enters a number greater than 100 
+// Cancels the input/enters an empty line.
+
+let num;
+
+do {
+  num = prompt("Enter a number greater than 100?", 0);
+} while (num <= 100 && num);
